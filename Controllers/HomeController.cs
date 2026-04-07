0ca64f6502aa33d9cdae7e7a5cca5b9f298b0a2b
AnatoleBoit <anatole.boittiaux@edu.esiee.fr>
2026-04-07 10:32:55 +0200

    Ajout Liste d'avis

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using TPLOCAL1.Models;
 
 //Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
@@ -21,8 +23,9 @@ public class HomeController : Controller
             //Call different pages, according to the id pass as parameter
             switch (id)
             {
-                case "OpinionList":
-                    return View(id);
+                case "AvisListe":
+                    List<Opinion> model = new OpinionList().GetAvis("./XmlFile/DataAvis.xml");
+                    return View(id, model);
                 case "Form":
                     return View(id);
                 default:
@@ -41,54 +44,90 @@ public class HomeController : Controller
         if (String.IsNullOrEmpty(form.Nom))
         {
             erreur = true;
+            ModelState.AddModelError("Nom", "Le Nom n'a pas été renseigné.");
         }
         if (String.IsNullOrEmpty(form.Prenom))
         {
             erreur = true;
+            ModelState.AddModelError("Prenom", "Le Prenom n'a pas été renseigné.");
         }
         if (String.IsNullOrEmpty(form.Genre))
         {
             erreur = true;
+            ModelState.AddModelError("Genre", "Le Genre n'a pas été renseigné.");
         }
         if (String.IsNullOrEmpty(form.Adresse))
         {
             erreur = true;
+            ModelState.AddModelError("Adresse", "L'Adresse n'a pas été renseignée.");
         }
         if (String.IsNullOrEmpty(form.CodePostal))
         {
             erreur = true;
+            ModelState.AddModelError("CodePostal", "Le Code Postal n'a pas été renseigné.");
+        }
+        if (form.CodePostal != null && !Regex.IsMatch(form.CodePostal, @"^[0-9]{5}$"))
+        {
+            erreur = true;
+            ModelState.AddModelError("CodePostal", "Le Code Postal doit être composé de 5 chiffres.");
         }
         if (String.IsNullOrEmpty(form.Ville))
         {
             erreur = true;
+            ModelState.AddModelError("Ville", "La Ville n'a pas été renseignée.");
         }
         if (String.IsNullOrEmpty(form.Email))
         {
             erreur = true;
+            ModelState.AddModelError("Email", "L'Adresse E-Mail n'a pas été renseignée.");
         }
-        /*if (false) //DateDebutForm
+        if (form.Email != null && IsInvalidEmail(form.Email))
         {
-            return null;
+            erreur = true;
+            ModelState.AddModelError("Email", "L'Adresse E-Mail n'est pas valide.");
+        }
+        if (form.DateDebutFormation == null)
+        {
+            erreur = true;
+            ModelState.AddModelError("DateDebutFormation", "La date n'a pas été renseignée.");
+        }
+        if (form.DateDebutFormation != null && form.DateDebutFormation > new DateOnly(2021,01,01)) //DateDebutForm
+        {
+            erreur = true;
+            ModelState.AddModelError("DateDebutFormation", "La date doit être inférieur au 01/01/2021.");
         }
         if (String.IsNullOrEmpty(form.FormationSuivie))
         {
-            Console.WriteLine(form.FormationSuivie);
-            return null;
-        }*/
+            erreur = true;
+            ModelState.AddModelError("FormationSuivie", "La Formation n'a pas été renseignée.");
+        }
 
         if (erreur)
         {
-            return null;
+            return View("Form");
         }
-        //TODO : test if model's fields are set
-        //if not, display an error message and stay on the form page
-        //else, call ValidationForm with the datas set by the user
-        return View("ValidationFormulaire", form);
 
+        return View("ValidationFormulaire", form);
     }
 
     public ActionResult ValidationFormulaire()
     {
         return View("ValidationFormulaire");
     }
+
+    private static bool IsInvalidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return true;
+        try
+        {
+            MailAddress addr = new MailAddress(email);
+            return addr.Address != email;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+
 }
